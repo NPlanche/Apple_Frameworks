6b05b3913e238da561552efb17f74acda1430ee0
NPlanche <ndelgadoplan2020@fau.edu>
2024-10-24 18:28:27 -0400

    Initial Commit

---
+++ Apple_Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple_Frameworks
+//
+//  Created by Nelly Delgado Planche on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
